4e63627d041ce412500c6429938947f1079e7bb0
Saeid Bostandoust <ssbostan@gmail.com>
2021-09-30 13:37:34 +0330

    adding examples

---
+++ examples/single/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    maas = {
+      source = "suchpuppet/maas"
+      version = "~> 3.1.3"
+    }
+  }
+}
+
+variable "MAAS_API_KEY" {
+  type = string
+}
+
+variable "MAAS_API_URL" {
+  type = string
+  default = "http://192.168.10.2:5240/MAAS"
+}
+
+provider "maas" {
+  api_version = "2.0"
+  api_key = var.MAAS_API_KEY
+  api_url = var.MAAS_API_URL
+}
+
+resource "maas_instance" "docker" {
+  release_erase = false
+  release_erase_quick = true
+}
